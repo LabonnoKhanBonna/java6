313ca399695913827a355617f6e2ef7c01030919
Labonno Khan Bonna <bonnalabonnokhan2708@gmail.com>
2023-10-25 21:11:30 +0600

    my second commit

---
+++ problem7.js
@@ -0,0 +1,28 @@
+// problem 6 finding bad data and good data
+function findBadData(arr){
+    if (!Array.isArray(arr)){
+        return "please provide me an array of number"
+    }
+
+    else{
+        let badData = 0;
+        for (let i = 0; i < arr.length; i++){
+            if (typeof arr[i] != "number"){
+                return "please provide me an array of number"
+            }
+            else{
+                if (arr[i] < 0){
+                    badData++;
+                }
+            }
+         
+        }
+        return badData;
+    }
+
+
+}
+
+const x = [1,2,3,4,5,6]
+console.log(findBadData(x))
+
